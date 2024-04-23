3f582b14cc4462f04c1debcb06ae0abdebb65546
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-04-23 11:41:13 -0600

    UPDATE: ProductRepository

---
+++ src/main/java/org/example/sprint1/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.example.sprint1.repository;
+
+import org.example.sprint1.entity.Product;
+
+import java.util.List;
+
+public class ProductRepository {
+    private List<Product> products;
+}
