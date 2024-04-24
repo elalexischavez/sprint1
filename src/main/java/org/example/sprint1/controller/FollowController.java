38d9077426a5f9e71ce355a0bcf6b5cfddac3280
FranciscoCardosoRodriguez <franciscocardosorodriguez@gmail.com>
2024-04-24 13:38:20 -0600

    ADD: Following Endpoint

--- src/main/java/org/example/sprint1/controller/FollowController.java
+++ src/main/java/org/example/sprint1/controller/FollowController.java
@@ -1,9 +1,11 @@
 package org.example.sprint1.controller;
 
+import org.example.sprint1.dto.FollowedSellersDTO;
 import org.example.sprint1.service.follow.IFollowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,10 @@ public class FollowController {
     @PostMapping("/users/{userId}/follow/{userIdToFollow}")
     ResponseEntity<?> userIdToFollow(@PathVariable int userId, @PathVariable int userIdToFollow) {
         return new ResponseEntity<>(followService.userIdToFollow(userId, userIdToFollow), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{userId}/followed/list")
+    public ResponseEntity<FollowedSellersDTO> getSellerFollowers(@PathVariable int userId) {
+        return new ResponseEntity<>(followService.getFollowedSellers(userId), HttpStatus.OK);
     }
 }
