bfea6eb0a547e54122809e5cb04bd7e6e556efd9
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-04-24 08:48:51 -0600

    UPDATE: SellerController

--- src/main/java/org/example/sprint1/controller/PostController.java
+++
@@ -1,7 +0,0 @@
-package org.example.sprint1.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class PostController {
-}
---
+++ src/main/java/org/example/sprint1/controller/SellerController.java
@@ -0,0 +1,29 @@
+package org.example.sprint1.controller;
+
+import org.example.sprint1.dto.RequestPostDTO;
+import org.example.sprint1.entity.Post;
+import org.example.sprint1.entity.Seller;
+import org.example.sprint1.service.seller.ISellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("")
+public class SellerController {
+
+    @Autowired
+    ISellerService postService;
+    @PostMapping("/products/post")
+    public ResponseEntity<Post> addPost(@RequestBody RequestPostDTO postDTO){
+        return new ResponseEntity<>(postService.addPost(postDTO), HttpStatus.OK);
+    }
+    @GetMapping("/products/list")
+    public ResponseEntity<List<Seller>> getAllSellers(){
+        return new ResponseEntity<>(postService.getSellers(), HttpStatus.OK);
+    }
+
+}
