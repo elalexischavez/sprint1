d3491a8012cbdfc2284f7c38a1e2af2ad524e27a
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-04-24 08:46:51 -0600

    UPDATE: ExceptionDTO

--- src/main/java/org/example/sprint1/dto/ExceptionDTO.java
+++ src/main/java/org/example/sprint1/dto/ExceptionDTO.java
@@ -1,5 +1,10 @@
 package org.example.sprint1.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ExceptionDTO {
     private String message;
 }
