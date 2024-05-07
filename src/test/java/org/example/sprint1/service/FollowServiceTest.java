46b0306e2032c309a4931569836b5e9b0ea64248
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-05-07 15:25:42 -0600

    Fix Merge Overrite

--- src/test/java/org/example/sprint1/service/FollowServiceTest.java
+++ src/test/java/org/example/sprint1/service/FollowServiceTest.java
@@ -1,5 +1,8 @@
 package org.example.sprint1.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.sprint1.dto.BasicSellerDTO;
+import org.example.sprint1.dto.FollowedSellersDTO;
 import org.example.sprint1.dto.SellerFollowerDto;
 import org.example.sprint1.entity.Customer;
 import org.example.sprint1.entity.Seller;
@@ -16,6 +19,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -154,5 +161,89 @@ public class FollowServiceTest {
         // Assert
         assertEquals("customerB", result.getFollowers().get(0).getUserName());
         assertEquals("customerA", result.getFollowers().get(1).getUserName());
+    }
+
+    @Test
+    @DisplayName("Validar que el orden de la lista de los vendedores que un usuario sigue este ASC")
+    public void getFollowedSellersTestAsc() {
+
+        // Arrange
+        List<Seller> sellers = List.of(
+                new Seller(1, "Juan", null, null),
+                new Seller(2, "Andres", null, null),
+                new Seller(3, "Diego", null, null)
+        );
+
+        List<BasicSellerDTO> sellersFollowed = Stream.of(
+                        new BasicSellerDTO(1, "Juan", null, null),
+                        new BasicSellerDTO(2, "Andres", null, null),
+                        new BasicSellerDTO(3, "Diego", null, null)
+                )
+                .sorted(Comparator.comparing(BasicSellerDTO::getSellerName)).collect(Collectors.toList());;
+
+        List<Integer> sellersIdFollowed = List.of(1, 2, 3);
+
+        Customer customer = new Customer();
+        customer.setUserId(1);
+        customer.setUserName("Customer1");
+        customer.setSellers(sellersIdFollowed);
+
+        FollowedSellersDTO expectedResponse = new FollowedSellersDTO();
+        expectedResponse.setUserId(customer.getUserId());
+        expectedResponse.setCustomerName(customer.getUserName());
+        expectedResponse.setFollowed(sellersFollowed);
+
+        // Act
+        ObjectMapper mapper = new ObjectMapper();
+
+        when(customerRepository.findCustomerById(1)).thenReturn(customer);
+        when(sellerRepository.getCustomersThatFollowsSellersById( 1 ) ).thenReturn(sellers);
+
+        FollowedSellersDTO response = followService.getFollowedSellers(1, "name_asc");
+
+        // Assert
+        Assertions.assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    @DisplayName("Validar que el orden de la lista de los vendedores que un usuario sigue este DESC")
+    public void getFollowedSellersTestDesc() {
+
+        // Arrange
+        List<Seller> sellers = List.of(
+                new Seller(1, "Juan", null, null),
+                new Seller(2, "Andres", null, null),
+                new Seller(3, "Diego", null, null)
+        );
+
+        List<BasicSellerDTO> sellersFollowed = List.of(
+                        new BasicSellerDTO(1, "Juan", null, null),
+                        new BasicSellerDTO(2, "Andres", null, null),
+                        new BasicSellerDTO(3, "Diego", null, null)
+                ).stream()
+                .sorted(Comparator.comparing(BasicSellerDTO::getSellerName).reversed()).collect(Collectors.toList());;
+
+        List<Integer> sellersIdFollowed = List.of(1, 2, 3);
+
+        Customer customer = new Customer();
+        customer.setUserId(1);
+        customer.setUserName("Customer1");
+        customer.setSellers(sellersIdFollowed);
+
+        FollowedSellersDTO expectedResponse = new FollowedSellersDTO();
+        expectedResponse.setUserId(customer.getUserId());
+        expectedResponse.setCustomerName(customer.getUserName());
+        expectedResponse.setFollowed(sellersFollowed);
+
+        // Act
+        ObjectMapper mapper = new ObjectMapper();
+
+        when(customerRepository.findCustomerById(1)).thenReturn(customer);
+        when(sellerRepository.getCustomersThatFollowsSellersById( 1 ) ).thenReturn(sellers);
+
+        FollowedSellersDTO response = followService.getFollowedSellers(1, "name_desc");
+
+        // Assert
+        Assertions.assertEquals(expectedResponse, response);
     }
 }
