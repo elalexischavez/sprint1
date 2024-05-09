1307c43f9b90b866cbdc811c3f02fc3f14be9b5d
Mauricio Jesus Pacheco <mauricio.pachecomijangos@mercadolibre.com.mx>
2024-05-09 11:37:47 -0600

    Added documentation with javadoc

--- src/main/java/org/example/sprint1/exception/ExceptionController.java
+++ src/main/java/org/example/sprint1/exception/ExceptionController.java
@@ -1,7 +1,6 @@
 package org.example.sprint1.exception;
 
 import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.ConstraintViolationException;
 import org.example.sprint1.dto.ExceptionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,25 +8,45 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.stream.Collectors;
 
+/**
+ * Controlador de excepciones para manejar y responder a las excepciones lanzadas en la aplicación.
+ */
 @RestControllerAdvice
 public class ExceptionController {
 
+    /**
+     * Maneja las excepciones de tipo NotFoundException.
+     *
+     * @param e La excepción lanzada.
+     * @return Una respuesta HTTP con el mensaje de la excepción y el estado HTTP NOT_FOUND.
+     */
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> notFound(NotFoundException e){
         ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
         return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * Maneja las excepciones de tipo BadRequestException.
+     *
+     * @param e La excepción lanzada.
+     * @return Una respuesta HTTP con el mensaje de la excepción y el estado HTTP BAD_REQUEST.
+     */
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> badRequest(BadRequestException e){
         ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Maneja las excepciones de tipo MethodArgumentNotValidException.
+     *
+     * @param e La excepción lanzada.
+     * @return Una respuesta HTTP con el mensaje de la excepción y el estado HTTP BAD_REQUEST.
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
         String errorMessage = e.getBindingResult().getFieldErrors().stream()
@@ -37,6 +56,12 @@ public class ExceptionController {
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Maneja las excepciones de tipo ConstraintViolationException.
+     *
+     * @param e La excepción lanzada.
+     * @return Una respuesta HTTP con el mensaje de la excepción y el estado HTTP BAD_REQUEST.
+     */
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<?> handleConstraintViolation(ConstraintViolationException e) {
         String errorMessage = e.getConstraintViolations().stream()
@@ -46,6 +71,12 @@ public class ExceptionController {
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Maneja las excepciones de tipo HttpMessageNotReadableException.
+     *
+     * @param e La excepción lanzada.
+     * @return Una respuesta HTTP con el mensaje de la excepción y el estado HTTP BAD_REQUEST.
+     */
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<?> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
         ExceptionDTO exceptionDto = new ExceptionDTO("Error al leer el cuerpo de la solicitud");
